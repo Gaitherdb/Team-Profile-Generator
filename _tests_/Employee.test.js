89c52a1eed8be9b9cb871fc4b445a51de077e848
David Gaither <gaitherdb@gmail.com>
2021-08-06 23:36:32 -0400

    created new versions of employee tests and created template htmls

--- _tests_/Employee.test.js
+++ _tests_/Employee.test.js
@@ -2,7 +2,7 @@ const Employee = require('../lib/Employee');
 
 describe("Employee class", () => {
     describe("getName method", () => {
-        it("Gets name input from Employee constructor", () => {
+        it("Gets name input from getName method", () => {
             const employee = new Employee("David", "1", "gaitherdb@gmail.com");
             const name = "David";
             const expectedResult = "David";
@@ -11,9 +11,32 @@ describe("Employee class", () => {
         });
     });
 
+    describe("getName method", () => {
+        it("Gets name input from getName method", () => {
+            const name = "David";
+            const employee = new Employee(name, "1", "gaitherdb@gmail.com");
+            expect(employee.getName()).toEqual(name);
+        });
+    });
 
-    describe("getID method", () => {
-        it("gets id input from Employee constructor", () => {
+    describe("Name contructor", () => {
+        it("Name contructor is properly set", () => {
+            const name = "David";
+            const employee = new Employee(name, "1", "gaitherdb@gmail.com");
+            expect(employee.name).toBe(name);
+        });
+    });
+
+    describe("getId method", () => {
+        it("gets id input from getId method", () => {
+            const id = "1";
+            const employee = new Employee("David", id, "gaitherdb@gmail.com");
+            expect(employee.getId()).toEqual(id);
+        });
+    });
+
+    describe("getId method", () => {
+        it("gets id input from getId method", () => {
             const employee = new Employee("David", "1", "gaitherdb@gmail.com");
             const id = "1";
             const expectedResult = "1";
@@ -21,13 +44,29 @@ describe("Employee class", () => {
             expect(actualResult).toEqual(expectedResult);
         });
     });
+
     describe("getEmail method", () => {
-        it("gets email input from Employee constructor", () => {
+        it("gets email input from getEmail method", () => {
+            const email = "gaitherdb@gmail.com";
+            const employee = new Employee("David", "1", email);
+            expect(employee.getEmail()).toEqual(email);
+        });
+    });
+
+    describe("getEmail method", () => {
+        it("gets email input from getEmail method", () => {
             const employee = new Employee("David", "1", "gaitherdb@gmail.com");
             const email = "gaitherdb@gmail.com";
             const expectedResult = "gaitherdb@gmail.com";
             const actualResult = employee.getEmail(email);
             expect(actualResult).toEqual(expectedResult);
+        });
+    });
+    describe("getRole method", () => {
+        it("gets employee role from inquirer", () => {
+            const employee = new Employee("David", "1", "gaitherdb@gmail.com");
+            const type = "Employee";
+            expect(employee.getRole()).toEqual(type);
         });
     });
 });
