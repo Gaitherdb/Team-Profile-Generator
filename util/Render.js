e7a15a48ef8835487a49e31662f7ac4c8ae756a2
David Gaither <gaitherdb@gmail.com>
2021-08-09 17:58:34 -0400

    fixed styles for the rendered HTML

--- util/Render.js
+++ util/Render.js
@@ -1,18 +1,20 @@
+//takes the array of created employees (team)
 function generateHTML(team) {
     var employees = [];
-
+    //from array of team members, filter out any that have a role manager, and for each of those, run the renderManager function
     employees.push(...team
         .filter(manager => manager.getRole() === "Manager")
         .map(manager => renderManager(manager)))
-
+    //from array of team members, filter out any that have a role engineer, and for each of those, run the renderEngineer function
     employees.push(...team
         .filter(engineer => engineer.getRole() === "Engineer")
         .map(engineer => renderEngineer(engineer)))
-
+    //from array of team members, filter out any that have a role intern, and for each of those, run the renderIntern function
     employees.push(...team
         .filter(intern => intern.getRole() === "Intern")
         .map(intern => renderIntern(intern)))
-
+    //employees variable now contains the rendered html for each selected type of employee
+    //take the employee variable, get rid of any commas within the array, and add that to the base HTML aka renderHTML
     let HTML = renderHTML(employees.join(""));
     return HTML;
 }
@@ -38,11 +40,8 @@ function renderHTML(cards) {
         </div>
     </div>
     <div class="container">
-        <div class="row">
-            <div class="col-4 d-flex justify-content-center">
+        <div class="row row-cols-auto d-flex justify-content-center">     
 ${cards}
-
-            </div>
         </div>
     </div>
 </body>
@@ -58,10 +57,10 @@ function renderManager(manager) {
     let officeNumber = manager.getOfficeNumber();
 
     return `
-   <div class="card" style="width: 18rem">
+   <div class="card col" style="width: 18rem; margin: 10px;">
                     <div class="card-header bg-primary text-light">
                         <h2 class="card-title">${name}</h2>
-                        <h3 class="card-title"><i class="fas fa-mug-hot"></i>${role}</h3>
+                        <h3 class="card-title"><i class="fas fa-mug-hot"></i> ${role}</h3>
                     </div>
                     <ul class="list-group list-group-flush">
                         <li class="list-group-item">ID: ${id}</li>
@@ -78,15 +77,15 @@ function renderEngineer(engineer) {
     let id = engineer.getId();
     let email = engineer.getEmail();
     let github = engineer.getGithub();
-    return `<div class="card" style="width: 18rem;">
+    return `<div class="card col" style="width: 18rem; margin: 10px;">
                     <div class="card-header bg-primary text-light">
                         <h2 class="card-title">${name}</h2>
-                        <h3 class="card-title"><i class="fas fa-glasses"></i>${role}</h3>
+                        <h3 class="card-title"><i class="fas fa-glasses"></i> ${role}</h3>
                     </div>
                     <ul class="list-group list-group-flush">
                         <li class="list-group-item">ID: ${id}</li>
                         <li class="list-group-item">Email: <a href="mailto:${email}">${email}</a></li>
-                        <li class="list-group-item">Github: <a href="https://github.com/${github}">${github}</li>
+                        <li class="list-group-item">Github: <a href="https://github.com/${github}">${github}</a></li>
                     </ul>
                 </div>
                 `
@@ -99,7 +98,7 @@ function renderIntern(intern) {
     let email = intern.getEmail();
     let school = intern.getSchool();
     return `
-    <div class="card" style="width: 18rem;">
+    <div class="card col" style="width: 18rem; margin: 10px;">
     <div class="card-header bg-primary text-light">
         <h2 class="card-title">${name}</h2>
         <h3 class="card-title"><i class="fas fa-user-graduate"></i> ${role}</h3>
